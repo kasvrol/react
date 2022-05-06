152c1144d0eb2eb7f93d8c02498b5d1eab8faef7
ana.karolina <ana.karolina@ablsystem.com.br>
2022-05-06 12:44:56 -0300

    feat: modal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 import './App.css';
+import { useState } from 'react';
+import Modal from 'react-modal';
 
 function App() {
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+
+  const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
+
   return (
     <div className="App">
-      AAAAAAAAAAAAAAAAAA
+      <button onClick={openModal}>Open Modal</button>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+      >
+        <h2>Hello</h2>
+        <div>I am a modal</div>
+        <button onClick={closeModal}>close</button>
+      </Modal>
     </div>
   );
 }
