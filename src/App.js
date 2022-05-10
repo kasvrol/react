bfa7cb8df2e76c5b07252ea5c3265e11de14309b
kasvrol <dovalekarolina@gmail.com>
2022-05-10 11:06:00 -0300

    refactor: separando funções do arquivo app.js para componentes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import "./App.css";
 import { useState } from "react";
+
+import Cow from "./components/Cow";
+import Rabbit from "./components/Rabbit";
+import Horse from "./components/Horse";
+
 import animal01 from "./images/cow.png";
 import animal02 from "./images/horse.png";
 import animal03 from "./images/rabbit.png";
+
+import "./App.css";
 
 function App() {
     const [modalIsOpen, setIsOpen] = useState(false);
 
     function toggleModal() {
         setIsOpen(!modalIsOpen);
+    }
+
+    function chooseAnimal(value) {
+        if (value === 'cow') {
+            return (
+                <Cow />
+            )
+        }
+        if (value === 'rabbit') {
+            return (
+                <Rabbit />
+            )
+        }
+        if (value === 'horse') {
+            return (
+                <Horse />
+            )
+        }
     }
 
     return (
@@ -23,19 +47,19 @@ function App() {
                         src={animal01}
                         alt="Vaca"
                         className="iconImage"
-                    // onClick={() => chooseAnimal("cow")}
+                        onClick={() => chooseAnimal("cow")}
                     />
                     <img
                         src={animal03}
                         alt="Ovelha"
                         className="iconImage"
-                    // onClick={() => chooseAnimal("rabbit")}
+                        onClick={() => chooseAnimal("rabbit ")}
                     />
                     <img
                         src={animal02}
                         alt="Cavalo"
                         className="iconImage"
-                    // onClick={() => chooseAnimal("horse")}
+                        onClick={() => chooseAnimal("horse")}
                     />
                 </div>
             ) : (
